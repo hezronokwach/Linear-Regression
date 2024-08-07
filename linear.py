bde33afd161a8d2c6d259abbc7ae74ade8a8f4e2
hokwach <hezronokwach@gmail.com>
2024-08-07 15:10:01 +0300

    Add logic for finding the gradient of the line regression line

--- linear.py
+++ linear.py
@@ -12,5 +12,18 @@ for index ,line  in enumerate(lines):
         yValues.append(number)
     except ValueError:
         continue
-print("x values:",xValues)
-print("y values", yValues)
+
+meanX = statistics.mean(xValues)
+meanY = statistics.mean(yValues)
+print("x mean:",meanX)
+print("y mean", meanY)
+tempGradient = 0
+tempC = 0
+for i in (xValues):
+    tempGradient += (xValues[i] - meanX) * (yValues[i]-meanY)
+    tempC += (xValues[i] - meanX) ** 2
+gradient = tempGradient / tempC
+c = meanY - (gradient * meanX)
+print("gradient", gradient)
+print("c", c)
+
